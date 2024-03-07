967df21adf67d5693baad7417eb5a7a9b07280de
Sunkavilli Suvarna raju <19A91F0018@aec.edu.in>
2024-03-07 11:41:19 +0530

    first commit

---
+++ src/main/java/in/proj1/App.java
@@ -0,0 +1,13 @@
+package in.proj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
